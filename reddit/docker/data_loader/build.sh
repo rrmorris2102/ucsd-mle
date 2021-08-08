150dd0fbe89173423e1edb94e64e14d357d04649
Robert Morrison <rrmorris@gmail.com>
2021-08-07 21:08:50 -0700

    Moved docker files to <root>/docker folder

--- reddit/docker/data_loader/build.sh
+++
@@ -1,2 +0,0 @@
-docker build --tag reddit-data-loader .
-docker tag reddit-data-loader:latest reddit-data-loader:v1.0.2
--- reddit/docker/data_loader/start.sh
+++
@@ -1,1 +0,0 @@
-docker run -dit --memory=4g --cpus="2.0" --name reddit-data-loader --env-file ../env reddit-data-loader:v1.0.2 
